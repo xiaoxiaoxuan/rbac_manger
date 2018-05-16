d51a39a7227df5bce8bbf757a377d3ab9a8c0a32
humingxuan <13625270510@163.com>
2018-05-16 11:47:54 +0800

    first

---
+++ src/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: humingxuan
+ * Date: 2018/5/16
+ * Time: 上午11:10
+ */
+
+namespace xiaoxuan;
+
+
+class Test
+{
+    public function index(){
+        echo 123;
+    }
+}
